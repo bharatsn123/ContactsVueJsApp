8dc930e2cab62be0abd53e7718f21f7eeeecd5b1
BharatNaganath <bharatsn123@gmail.com>
2024-12-15 21:08:09 +1100

    Comments added to AzureMap.vue

--- contactsapp/src/AzureMap.vue
+++ contactsapp/src/AzureMap.vue
@@ -34,21 +34,26 @@ export default {
   },
   methods: {
     initializeMap() {
-      const map = new atlas.Map(this.mapId, {
-        center: [this.longitude, this.latitude],
-        zoom: this.zoom,
-        style: "road",
+      // Create a new Azure Map instance
+      const map = new atlas.Map(this.mapId, { 
+        center: [this.longitude, this.latitude], // Set the initial center of the map using provided longitude and latitude
+        zoom: this.zoom, 
+        style: "road", // Define the map's visual style 
         authOptions: {
-          authType: "subscriptionKey",
+          authType: "subscriptionKey", // Specify the authentication method (using a subscription key here)
           subscriptionKey: this.subscriptionKey,
         },
       });
 
+      // Add an event listener for the "ready" event, triggered when the map finishes loading
       map.events.add("ready", () => {
+        // Create a new marker using Azure Maps' HtmlMarker feature
         const marker = new atlas.HtmlMarker({
-          position: [this.longitude, this.latitude],
-          text: "📍",
+          position: [this.longitude, this.latitude], /
+          text: "📍", 
         });
+
+        // Add the marker to the map
         map.markers.add(marker);
       });
     },
